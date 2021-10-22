51b37bf6e9ec49bdc563368ad90b58064892c0c9
Amine13u <46264764+Amine13u@users.noreply.github.com>
2021-10-22 22:07:23 +0100

    Fixing test

--- tests/unit/components/Form.spec.js
+++ tests/unit/components/Form.spec.js
@@ -1,4 +1,4 @@
-import { mount, createLocalVue } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Form from "@/components/Form.vue";
 // import Vue from "vue";
 import Vuex from "vuex";
@@ -12,10 +12,34 @@ const localVue = createLocalVue();
 describe("Form", () => {
   test("snapshot", () => {
     let $store = {
-      state: {},
+      state: {
+        search: "",
+        dialog: false,
+        editedUser: {},
+        users: [
+          {
+            id: 1,
+            firstName: "Amine",
+            lastName: "Mannai",
+            email: "amine@gmail.com",
+          },
+          {
+            id: 2,
+            firstName: "Karim",
+            lastName: "Gharbi",
+            email: "karim@gmail.com",
+          },
+          {
+            id: 3,
+            firstName: "John",
+            lastName: "Doe",
+            email: "john@gmail.com",
+          },
+        ],
+      },
     };
 
-    const wrapper = mount(Form, {
+    const wrapper = shallowMount(Form, {
       mocks: { $store },
     });
     expect(wrapper).toMatchSnapshot();
