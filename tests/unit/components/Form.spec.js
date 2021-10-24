0003143a43fc085076dbfb1363680d98253f07af
Amine13u <46264764+Amine13u@users.noreply.github.com>
2021-10-24 23:55:57 +0100

    Adding test

--- tests/unit/components/Form.spec.js
+++ tests/unit/components/Form.spec.js
@@ -1,38 +1,43 @@
 import { shallowMount } from "@vue/test-utils";
 import Form from "@/components/Form.vue";
 
-describe("Form", () => {
-  test("snapshot", () => {
-    let $store = {
-      state: {
-        search: "",
-        dialog: false,
-        editedUser: {},
-        users: [
-          {
-            id: 1,
-            firstName: "Amine",
-            lastName: "Mannai",
-            email: "amine@gmail.com",
-          },
-          {
-            id: 2,
-            firstName: "Karim",
-            lastName: "Gharbi",
-            email: "karim@gmail.com",
-          },
-          {
-            id: 3,
-            firstName: "John",
-            lastName: "Doe",
-            email: "john@gmail.com",
-          },
-        ],
+let $store = {
+  state: {
+    search: "",
+    dialog: false,
+    editedUser: {},
+    users: [
+      {
+        id: 1,
+        firstName: "Amine",
+        lastName: "Mannai",
+        email: "amine@gmail.com",
       },
-    };
-    const wrapper = shallowMount(Form, {
-      mocks: { $store },
-    });
+      {
+        id: 2,
+        firstName: "Karim",
+        lastName: "Gharbi",
+        email: "karim@gmail.com",
+      },
+      {
+        id: 3,
+        firstName: "John",
+        lastName: "Doe",
+        email: "john@gmail.com",
+      },
+    ],
+  },
+};
+
+const wrapper = shallowMount(Form, {
+  mocks: { $store },
+});
+
+describe("Form unit test : ", () => {
+  test("snapshot", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  test("Is a Vue instance", () => {
+    expect(wrapper.vm).toBeTruthy();
   });
 });
